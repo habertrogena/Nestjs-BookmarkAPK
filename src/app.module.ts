33a9c131252ccd5b0d8ac90094636780b204331f
habertrogena <habertorogy12@gmail.com>
2023-01-10 18:08:46 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
+  
 })
 export class AppModule {}
+
+
+//bookmark application.
+// authentication,crud operations, users.
